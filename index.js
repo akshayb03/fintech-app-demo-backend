81d53dc03cb5938d79a04f41cc9ea33120444af6
akshayb03 <akshaybhati003@gmail.com>
2025-08-07 10:14:01 +0530

    structure changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import Account from './models/Account.js';
+import Account from './src/models/Account.js';
 import cors from 'cors'
 
 dotenv.config();
